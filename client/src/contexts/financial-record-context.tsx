af36a9170c99deb867e4593b50df086649303872
Sakshi Kumari <sakshikporwal@gmail.com>
2025-05-08 12:39:44 +0530

    Fetch and store user-specific financial records in context; verified via console.log

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -1,5 +1,6 @@
 // this is state manager in this application where all our state and func will exist
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { useUser } from '@clerk/clerk-react'
 
 
 // use interface that context will contain, that is actually same as we use in schema so copy from there
@@ -32,6 +33,25 @@ interface FinancialRecord {
     }) => {
       // create state
       const [records, setRecords] = useState<FinancialRecord[]>([]);
+      const { user } = useUser(); // use useUser that we get user from clerk
+      
+      // now add this function for showing the list of database
+      const fetchRecord = async () => {
+        if(!user) return;
+        const response = await fetch(
+          `http://localhost:3001/financial-records/getAllByUserID/${user.id}`
+        );
+
+        if (response.ok) {
+          const records = await response.json(); // if response is ok then this will fetch the record
+          console.log(records);
+          setRecords(records);
+        }
+      };
+
+      useEffect(() => {
+        fetchRecord();
+      }, [user]);
 
       const addRecord = async (record: FinancialRecord) => {
         // fetch api i.e. running on the local host so link write like this..http...and port that we have defined 3001 in sever file etc and display records
