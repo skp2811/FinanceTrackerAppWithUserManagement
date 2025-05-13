a69743f296bf049a070080b13472d44aee10bf95
Sakshi Kumari <sakshikporwal@gmail.com>
2025-05-13 23:09:27 +0530

    add basic CSS styling to UI components

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { useUser } from "@clerk/clerk-react"; 
 import { FinancialRecordForm } from "./financial-record-form";
 import { FinancialRecordList } from "./financial-record-list";
+// import css file here
+import "./financial-record.css";
 
 export const Dashboard = () => { 
     const { user } = useUser();
